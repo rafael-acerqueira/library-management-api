03b04d75e6f61dabba8aca432577c2c8facb2359
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-07-02 09:31:27 -0300

    Setup project

---
+++ README.md
@@ -0,0 +1,10 @@
+## Setup Project
+
+ `docker compose build `
+
+ `docker compose run web rails db:create`
+
+ `docker compose up`
+
+## Acess project
+ The base URL is `http://localhost:3001/`
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'register'
+  },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+
+  namespace :api do
+    namespace :v1 do
+      resources :books
+    end
+  end
+end
