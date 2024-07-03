794f72d3247316b7e162bdf0c100fcf17705cb2a
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-07-03 20:12:05 -0300

    Update README and seeds

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
  `docker compose build `
 
- `docker compose run web rails db:create db:seed`
+ `docker compose run web rails db:create db:migrate db:seed`
 
  `docker compose up`
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,19 @@
 User.destroy_all
 Book.destroy_all
+BorrowedBook.destroy_all
 
 
-User.create( email: Faker::Internet.email,
+librarian = User.create( email: Faker::Internet.email,
              password: '123456',
              librarian: true
             )
 
-User.create( email: Faker::Internet.email,
+member = User.create( email: Faker::Internet.email,
+            password: '123456',
+            librarian: false
+            )
+
+user = User.create( email: Faker::Internet.email,
             password: '123456',
             librarian: false
             )
@@ -21,3 +27,11 @@ User.create( email: Faker::Internet.email,
     total_copies:  Faker::Number.within(range: 1..10)
   )
 end
+
+books = Book.all
+
+BorrowedBook.create(book: books[0], user: member, borrowing_date: '2024-06-01', due_date: '2024-06-16')
+BorrowedBook.create(book: books[1], user: member, borrowing_date: '2024-06-15', due_date: '2024-06-30', returned: true)
+BorrowedBook.create(book: books[2], user: user, borrowing_date: '2024-06-01', due_date: '2024-06-16')
+BorrowedBook.create(book: books[3], user: user, borrowing_date: '2024-07-01', due_date: '2024-07-16')
+BorrowedBook.create(book: books[4], user: user, borrowing_date: '2024-07-01', due_date: Date.today)
