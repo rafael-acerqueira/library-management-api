21a0f6584874b50d24abc310baca1a2a2a2f568a
Rafael Aquino <rafael.acerqueirah@gmail.com>
2024-07-03 20:18:49 -0300

    Consider librarian field when register a new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,17 @@ class ApplicationController < ActionController::API
 
   before_action :authenticate_user!
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   def is_librarian?
     render json: { message: 'librarian only, not authorized' }, status: :unauthorized unless current_user.librarian?
+  end
+
+
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[librarian])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[librarian])
   end
 end
